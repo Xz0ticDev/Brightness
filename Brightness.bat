448d597507524860246ef0d2c53de6ebdc714864
Alex Troiani <atroiani6@gmail.com>
2016-10-08 18:31:33 -0700

    Update Brightness.bat

--- Brightness.bat
+++ Brightness.bat
@@ -1,5 +1,5 @@
 @echo off
 color 0a
 set /p brightnessLevel=Enter Desired Brightness: 
-"C:\Program Files (x86)\ScreenBright\ScreenBright.exe" -set brightness %brightnessLevel%
+ScreenBright.exe -set brightness %brightnessLevel%
 exit
