fd54212a92d067f8de5f66307645c09df36762e0
Alex Troiani <atroiani6@gmail.com>
2017-06-12 21:32:13 -0700

    Update Brightness.bat
    now saves brightness setting and reapplies it on windows startup

--- Brightness.bat
+++ Brightness.bat
@@ -1,5 +1,14 @@
 @echo off
 color 0a
+
+setlocal enableextensions disabledelayedexpansion
+
+set location="C:\Users\Purple\Desktop\savedBrightness.txt"
 set /p brightnessLevel=Enter Desired Brightness: 
-ScreenBright.exe -set brightness %brightnessLevel%
+
+"C:\Program Files (x86)\ScreenBright.exe" -set brightness %brightnessLevel%
+
+cd.>%location%
+@echo %brightnessLevel%>>%location%
+
 exit
